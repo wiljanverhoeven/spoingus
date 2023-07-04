8411f43f899a1fb67ac9d9e86f2f991cf28de076
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2023-07-04 10:01:45 +0200

    added some comments

--- game.js
+++ game.js
@@ -1,92 +1,82 @@
-//score
-let board;
-let boardWidth = 750;
-let boardHeight = 250;
-let context;
+//set up the board
+let board; //defines the board as a variable
+let boardWidth = 750; //board height
+let boardHeight = 250; //board width
+let context;//used for drawing on the board
 
-//spoingus
-let spoingusWidth = 88;
-let spoingusHeight = 94;
-let spoingusX = 50;
-let spoingusY = boardHeight - spoingusHeight;
-let spoingusimg;
-
-let spoingus = {
-    x : spoingusX,
-    y : spoingusY,
-    width : spoingusWidth,
-    height : spoingusHeight
+//set up spoingus the cat
+let spoingusWidth = 88; //spoingus width
+let spoingusHeight = 94;   //spoingus height
+let spoingusX = 50;     //spoingus x position
+let spoingusY = boardHeight - spoingusHeight; //spoingus y position
+let spoingusimg; //defines spoingus image
+let spoingus = { //spoingus object
+    x : spoingusX, //spoingus x position
+    y : spoingusY, //spoingus y position
+    width : spoingusWidth, //spoingus width
+    height : spoingusHeight //spoingus height
 }
 
-//cucumbers
-let cucumbers = [];
-
-let cucumber1Width = 34;
-let cucumber2Width = 69;
-let cucumber3Width = 102;
-
-let cucumberHeight = 70;
-let cucumberX = 700;
-let cucumberY = boardHeight - cucumberHeight;
-
-let cucumber1Img;
-let cucumber2Img;
-let cucumber3Img;
+//set up cucumbers
+let cucumbers = []; //array of five cucumbers
+let cucumber1Width = 34; //cucumber1 width
+let cucumber2Width = 69; //cucumber2 width
+let cucumber3Width = 102; //cucumber3 width
+let cucumberHeight = 70; //cucumbers height
+let cucumberX = 700; //cucumbers x position
+let cucumberY = boardHeight - cucumberHeight; //cucumbers y position
+let cucumber1Img; //defines cucumber1 image
+let cucumber2Img; //defines cucumber2 image
+let cucumber3Img; //defines cucumber3 image
 
 //physics
-
 let velocityX = -8; //cucumber movement speed
-let velocityY = 0;
-let gravity = .4;
+let velocityY = 0;// spoingus jump hieght
+let gravity = .4;// spoingus fall speed
 
+//user interface variables
+let gameOver = false; //game over boolean
+let score = 0; //defines score
+let pause = false; //pause boolean
 
-let gameOver = false;
-let score = 0;
-let pause = false;
-
-document.getElementById("start").onclick = playGame;
-
-function resumeGame() {
-    pause = false;
-    document.getElementById("resume").style.display = "none";
-    document.getElementById("pause").style.display = "inline";
-    document.getElementById("audioplayer").play();
-    update();
+function pauseGame() { //when the pause button is clicked the game will pause.
+    pause = true; //sets pause boolean to true
+    document.getElementById("resume").style.display = "inline"; //displays resume button
+    document.getElementById("pause").style.display = "none"; //hides pause button
+    document.getElementById("audioplayer").pause(); //pauses background music
 }
 
-
-function pauseGame() {
-    pause = true;
-    document.getElementById("resume").style.display = "inline";
-    document.getElementById("pause").style.display = "none";
-    document.getElementById("audioplayer").pause();
+function resumeGame() { //when the game is paused a resume button will appear and when clicked the game will resume.
+    pause = false; //sets pause boolean to false
+    document.getElementById("resume").style.display = "none"; //hides resume button
+    document.getElementById("pause").style.display = "inline"; //displays pause button
+    document.getElementById("audioplayer").play(); //plays background music
+    update(); //runs the update function and resumes the game
 }
 
-
-function playGame() {	
-    document.getElementById("hold").style.display = "inline";
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    
+function playGame() {	//when the start button is clicked the game will start.
+    document.getElementById("hold").style.display = "inline"; //displays pause button
+    board = document.getElementById("board"); //sets board to the board id from the html
+    board.height = boardHeight; //sets board height
+    board.width = boardWidth; //sets board width
 
     context = board.getContext("2d"); //used for drawing on the board
      
     //load images
-    spoingusimg = new Image();
-    spoingusimg.src = "images/spoingus.png";
-    spoingusimg.onload = function () {
-        context.drawImage(spoingusimg, spoingus.x, spoingus.y, spoingus.width, spoingus.height);
+    spoingusimg = new Image(); //defines spoingus image
+    spoingusimg.src = "images/spoingus.png"; //sets spoingus image source
+    spoingusimg.onload = function () { //when spoingus image loads it will be drawn on the board
+        context.drawImage(spoingusimg, spoingus.x, spoingus.y, spoingus.width, spoingus.height); 
     }
 
-    cucumber1Img = new Image();
-    cucumber1Img.src = "images/cucumber.png";
+    cucumber1Img = new Image(); //defines cucumber1 image
+    cucumber1Img.src = "images/cucumber.png"; //sets cucumber1 image source
 
-    cucumber2Img = new Image();
-    cucumber2Img.src = "images/cucumber2.png";
+    cucumber2Img = new Image(); //defines cucumber2 image
+    cucumber2Img.src = "images/cucumber2.png"; //sets cucumber2 image source
 
-    cucumber3Img = new Image();
-    cucumber3Img.src = "images/cucumber3.png";
+    cucumber3Img = new Image(); //defines cucumber3 image
+    cucumber3Img.src = "images/cucumber3.png"; //sets cucumber3 image source
 
     requestAnimationFrame(update);
     setInterval(placeCucumbers, 1000); //1000 milliseconds = 1 second
